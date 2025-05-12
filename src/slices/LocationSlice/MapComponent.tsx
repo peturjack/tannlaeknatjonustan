a301ac83e1e851843e09c446e97ef97a1bb8ed13
Petur jack <peturwj@gmail.com>
2025-05-12 22:05:10 +0000

    fixing map

--- src/slices/LocationSlice/MapComponent.tsx
+++ src/slices/LocationSlice/MapComponent.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
+import L, { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 // Fix for default marker icon
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+const DefaultIcon = L.Icon.Default.prototype as unknown as {
+  _getIconUrl?: string;
+};
+delete DefaultIcon._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
