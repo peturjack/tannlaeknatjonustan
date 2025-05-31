ec036564915c6de59f8f26eaee3081e06ff134d9
Petur jack <peturwj@gmail.com>
2025-05-31 19:54:51 +0000

    fixed eslink with form

--- src/slices/Form/Form.tsx
+++ src/slices/Form/Form.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { KeyTextField, LinkField } from "@prismicio/client";
-import { PrismicNextLink } from "@prismicio/next";
+
 import React, { useState } from "react";
 
 type Props = {
