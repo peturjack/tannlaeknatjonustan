1d7ecbb721cf29d6106d6cddd79c9bd628f31685
Petur jack <peturwj@gmail.com>
2025-06-04 20:18:36 +0000

    fixed out team slice and fixed the animation for the mobile nav

--- src/app/components/nav/Nav.tsx
+++ src/app/components/nav/Nav.tsx
@@ -80,9 +80,9 @@ const Nav = ({ nav }: Props) => {
               {nav.data.navlinks.map((item, index) => (
                 <motion.div
                   key={index}
-                  initial={{ opacity: 0, y: 50 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 50 }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
                   transition={{ duration: 0.5, ease: "easeInOut" }}
                 >
                   <PrismicNextLink
--- src/slices/OurTeamSlice/index.tsx
+++ src/slices/OurTeamSlice/index.tsx
@@ -20,28 +20,31 @@ const OurTeamSlice: FC<OurTeamSliceProps> = ({ slice }) => {
         data-slice-type={slice.slice_type}
         data-slice-variation={slice.variation}
       >
-        <div className="text-center text-primary-600 max-w-2xl">
+        <div className="text-center text-primary-600 max-w-2xl mx-auto">
           <h2>{slice.primary.title}</h2>
-          <p className="max-w-4xl text-pretty leading-relaxed">
+          <p className="max-w-4xl text-pretty text-lg leading-relaxed">
             {slice.primary.description}
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-12">
           {slice.primary.add_staff.map((item, index) => (
             <div
               key={index}
-              className="flex items-center h-[110%] gap-4 text-primary-700"
+              className="flex items-center gap-6 text-primary-700"
             >
-              <div className="bg-secondary-700  relative inset-0 h-full w-40 rounded-4xl ">
+              {/* Green background with image overflowing bottom */}
+              <div className="relative bg-secondary-700 flex items-end justify-center w-40 h-40 rounded-3xl shrink-0 overflow-visible">
                 <PrismicNextImage
-                  height={100}
-                  width={100}
-                  className="object-cover absolute inset-x-0 mx-auto bottom-0"
+                  height={200}
+                  width={112}
+                  className="object-cover w-28 h-45 rounded-2xl absolute left-1/2 -translate-x-1/2 bottom-0"
                   field={item.image}
+                  style={{ zIndex: 1 }}
                 />
               </div>
-              <div className="flex flex-col gap-1">
-                <h3 className="text-lg">{item.name}</h3>
+              {/* Staff info */}
+              <div className="flex flex-1 flex-col gap-1">
+                <h3 className="text-lg font-semibold">{item.name}</h3>
                 <span>{item.occupation}</span>
                 <span>{item.workplace}</span>
                 <span>{item.email}</span>
